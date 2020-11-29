a85d840cc5b965891e8563262e8847a12049b7dc
ronlevy <ronlevy12000@gmail.com>
2020-11-29 15:58:05 +0200

    logging added

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from myconstants import *
 from passw import *
 from save_to_database import SaveToDatabase
 from scraper import Scraper
+import logging
+
+logging.basicConfig(handlers=[logging.FileHandler('scraping.log', 'w', 'utf-8')],
+                    format="%(asctime)s-%(levelname)s-FILE:%(filename)s-FUNC:%(funcName)s-LINE:%(lineno)d-%(message)s",
+                    datefmt='%m-%d %H:%M',
+                    level=logging.INFO)
 
 
 class Main:
@@ -16,8 +22,12 @@ class Main:
         and the limits on the search
         """
         self.ar = ArgParseInput()
+        logging.info(f'An ARgParseInput object was successfully made')
         self.places = self.ar.argp()[0]  # scrap those places
+        logging.info(f'A places instance was successfully made out of ARgParseInput')
+        logging.debug(f'Places len: {len(self.places)}')
         self.search_limit = self.ar.argp()[1]
+        logging.debug(f'Search limit: {self.search_limit}')
         if self.search_limit == 9999:
             self.search_limit = 'Unlimited search'
         print(f"limit pages up to page number: {self.search_limit}")
@@ -31,13 +41,16 @@ class Main:
         """
         if not os.path.exists(name):
             os.mkdir(name)
+            logging.info(f'A folder has been made. Folder name: {name}')
 
     def run(self):
         """Running the program with the imported modules"""
         for self.place in self.places:
             print(f"Looking for results in: {self.place}")
             self.sc = Scraper(place=self.place, path_to_driver=WEBDRIVER_PATH)
+            logging.info(f'An instance was successfully made out of Scraper')
             self.db = SaveToDatabase(self.sc)
+            logging.info(f'An instance was successfully made out of SavetToDatabase')
             self.sc.create_driver()
             self.make_folder(self.place)
             self.url = HOMEPAGE + self.place + '/' + "list_v"
@@ -62,15 +75,19 @@ class Main:
             os.mkdir(os.path.join(self.place, self.page))
         urls = self.sc.get_urls()  # List of all the url in that page
         for idx, cell_url in enumerate(urls):
-            print(f"Page number: {self.page}")
+            logging.info(f'Url from page is under proccess')
+            logging.debug(f"Page number: {self.page}")
             inner_folder = self.sc.make_inner_folder(idx, cell_url, self.page)
-            print(f"Image folder name:{inner_folder}")
+            logging.debug(f"Image folder name:{inner_folder}")
             self.sc.driver_get(cell_url)
             self.sc.get_image(inner_folder=inner_folder, page=self.page)
+            logging.debug(f"The photos has been downloaded")
             print(self.sc.info_data())
+            logging.debug(f"Info data has been processed. len: {len(self.sc.info_data())}")
             print(self.sc.table_data())
+            logging.debug(f"Table data has been processed. len: {len(self.sc.table_data())}")
             self.db.add_data_to_database()  # Making SQL Query from the data
-            print("Success!")  # Should print if there was no error
+            logging.info(f"Success! data has been inserted into SQL tables")
         self.sc.driver_get(self.url)
         print(f"End of loop {self.page}")
         self.page = int(self.page)
--- scraper.py
+++ scraper.py
@@ -9,7 +9,12 @@ from selenium.webdriver.common.by import By
 
 from myconstants import *
 from passw import *
+import logging
 
+logging.basicConfig(handlers=[logging.FileHandler('scraping.log', 'w', 'utf-8')],
+                    format="%(asctime)s-%(levelname)s-FILE:%(filename)s-FUNC:%(funcName)s-LINE:%(lineno)d-%(message)s",
+                    datefmt='%m-%d %H:%M',
+                    level=logging.INFO)
 
 class Scraper:
 
@@ -17,11 +22,12 @@ class Scraper:
         """Initialize Scraper class. Define place and path to the web driver"""
         self.place = place
         self.path_to_driver = path_to_driver
+        logging.info(f'A scraper object was successfully made. Place: {self.place}')
 
     def create_driver(self):
         """Creates a driver with the path initialized at the init method"""
         self.driver = webdriver.Chrome(executable_path=self.path_to_driver)
-
+        logging.info(f'A driver object was successfully made')
     def get_image(self, page, inner_folder):
         """
         Downloading images from a given site.
@@ -41,16 +47,22 @@ class Scraper:
             file_name = f"img_{j}." + extension
             try:
                 urllib.request.urlretrieve(img, os.path.join(self.place, page, inner_folder, file_name))
-            except ValueError:
+                logging.info(f'An image was successfully downloaded. File name: {file_name}')
+            except ValueError as e:
+                logging.critical(f'The following error has occurred:{e}')
                 pass
 
     def tables_len(self):
         """return the len of a Selenium object"""
-        return len(self.driver.find_elements(By.TAG_NAME, "td"))
+        table_len = len(self.driver.find_elements(By.TAG_NAME, "td"))
+        logging.debug(f'Table length: {table_len}')
+        return table_len
+
 
     def driver_get(self, url):
         """Update the driver to the received url"""
         self.driver.get(url)
+        logging.info(f'driver.get was successfully made on the following url: {url}')
 
     def get_urls(self):
         """
@@ -59,6 +71,7 @@ class Scraper:
         """
         page_urls = self.driver.find_elements_by_css_selector(".tAddress [href]")
         urls = [cell.get_attribute('href') for cell in page_urls]  # All urls in that page
+        logging.debug(f'Urls list length: {len(urls)}')
         return urls
 
     def make_inner_folder(self, idx, cell_url, page):
@@ -72,13 +85,17 @@ class Scraper:
         inner_folder = str(idx + 1) + '__' + cell_url[HALF_URL:]
         if not os.path.exists(os.path.join(self.place, page, inner_folder)):
             os.mkdir(os.path.join(self.place, page, inner_folder))
+        logging.debug(f'Returned folder name: {inner_folder}')
         return inner_folder
 
     def full_address(self):
         """Return the house owner full address. Converting Selenium object to text"""
         try:
-            return self.driver.find_element_by_class_name('fullAddress').text
-        except NoSuchElementException:
+            full_address = self.driver.find_element_by_class_name('fullAddress').text
+            logging.info(f'Successfully returned address : {full_address}')
+            return full_address
+        except NoSuchElementException as e:
+            logging.critical(f'The following error has occurred:{e}')
             return
 
     def just_listed_status(self):
@@ -87,67 +104,99 @@ class Scraper:
         return True if it just listed, False otherwise
         """
         try:
-            if self.driver.find_element_by_class_name('justlisted').text == "Status: Just Listed":
+            just_listed = self.driver.find_element_by_class_name('justlisted').text == "Status: Just Listed"
+            if just_listed:
                 return True
             else:
                 return False
-        except NoSuchElementException:
+            logging.info(f'Successfully returned boolean regarding just_listed')
+            logging.debug(f'just_listed status: {just_listed}')
+        except NoSuchElementException as e:
+            logging.critical(f'The following error has occurred:{e}')
             return
 
     def reo_id(self):
         """return the reo_id of the property as a string"""
         try:
+            logging.info(f'Successfully returned on reo_id')
             return self.driver.find_element_by_class_name('reoid').text.split(':')[1]
-        except NoSuchElementException:
+        except NoSuchElementException as e:
+            logging.critical(f'The following error has occurred:{e}')
             return
 
     def mls_id(self):
         """return the mls_id of the property as a string"""
         try:
+            logging.info(f'Successfully returned on mls_id')
             return self.driver.find_element_by_class_name('mlsid').text.split(':')[1]
-        except NoSuchElementException:
+        except NoSuchElementException as e:
+            logging.critical(f'The following error has occurred:{e}')
             return
 
     def agent_name(self):
         """return string of the name of the real-estate agent who's selling the house"""
         try:
-            return self.driver.find_element_by_class_name('agent-name').text
-        except NoSuchElementException:
+            agent_name = self.driver.find_element_by_class_name('agent-name').text
+            logging.info(f'Successfully returned on agent_name')
+            logging.debug(f'Agent name: {agent_name}')
+            return agent_name
+        except NoSuchElementException as e:
+            logging.critical(f'The following error has occurred:{e}')
             return
 
     def agent_phone(self):
         """return string of the phone number of the real-estate agent who's selling the house"""
         try:
-            return self.driver.find_element_by_class_name('_agent-phone').text.split(':')[1]
-        except NoSuchElementException:
+            agent_phone = self.driver.find_element_by_class_name('_agent-phone').text.split(':')[1]
+            logging.info(f'Successfully returned on agent_phone')
+            logging.debug(f'Agent phone: {agent_phone}')
+            return agent_phone
+        except NoSuchElementException as e:
+            logging.critical(f'The following error has occurred:{e}')
             return
 
     def agent_company_name(self):
         """return string of the name of the real-estate company which offer the house for sale"""
         try:
-            return self.driver.find_element_by_class_name('company-name').text
-        except NoSuchElementException:
+            company_name = self.driver.find_element_by_class_name('company-name').text
+            logging.info(f'Successfully returned on agent_company_name')
+            logging.debug(f'Company name: {company_name}')
+            return company_name
+        except NoSuchElementException as e:
+            logging.critical(f'The following error has occurred:{e}')
             return
 
     def agent_company_phone(self):
         """return string of the phone number of the real-estate company which offer the house for sale"""
         try:
-            return self.driver.find_element_by_class_name('company-phone').text
-        except NoSuchElementException:
+            company_phone = self.driver.find_element_by_class_name('company-phone').text
+            logging.info(f'Successfully returned on agent_company_phone')
+            logging.debug(f'Company phone: {company_phone}')
+            return company_phone
+        except NoSuchElementException as e:
+            logging.critical(f'The following error has occurred:{e}')
             return
 
     def agent_company_address(self):
         """Return string of the address of the real-estate company which offer the house for sale"""
         try:
-            return self.driver.find_element_by_class_name('company-address').text
-        except NoSuchElementException:
+            company_address = self.driver.find_element_by_class_name('company-address').text
+            logging.info(f'Successfully returned on agent_company_address')
+            logging.debug(f'Company address: {company_address}')
+            return
+        except NoSuchElementException as e:
+            logging.critical(f'The following error has occurred:{e}')
             return
 
     def description(self):
         """Return a string of the house description"""
         try:
-            return self.driver.find_element_by_class_name('class="description-body').text
-        except NoSuchElementException:
+            desc = self.driver.find_element_by_class_name('class="description-body').text
+            logging.info(f'Successfully returned on description')
+            logging.debug(f'Description length: {len(desc)}')
+            return desc
+        except NoSuchElementException as e:
+            logging.critical(f'The following error has occurred:{e}')
             return
 
     def info_data(self):
@@ -155,6 +204,7 @@ class Scraper:
         Get the informative data regarding the propary
         :return: list of table of relevant data
         """
+        logging.info(f'info_data has been Successfully called')
         return self.full_address(), self.just_listed_status(), \
                self.reo_id(), self.mls_id(), self.agent_name(),\
                self.agent_phone(), self.agent_company_name(), \
@@ -186,6 +236,7 @@ class Scraper:
             elif a == 'Year Built':
                 b = datetime.strptime(b, '%Y')
             data_dict.update({a: b})
+        logging.info(f'table_data has been Successfully called')
         return data_dict
 
 

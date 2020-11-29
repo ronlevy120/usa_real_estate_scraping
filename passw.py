9579b4d6c74c008e069e19918d9709cb45c5b061
ronlevy <ronlevy12000@gmail.com>
2020-11-29 16:14:17 +0200

    logging added

---
+++ passw.py
@@ -0,0 +1,6 @@
+DB_FILENAME = 'usa_scraping_database'
+DB_PATH = r"C:\Users\user\Desktop\ITC\course\project data mining\usa_real_estate_scraping\usa_scraping_database.db"
+HOST = "localhost"
+USER = "root"
+PASS = "ronlevy95"
+WEBDRIVER_PATH = r"C:\Users\user\Documents\the_webdriver\chromedriver.exe"

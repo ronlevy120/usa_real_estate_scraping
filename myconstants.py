cb4694db320e8cdf828d699ebeef3f88464840b5
ronlevy <ronlevy12000@gmail.com>
2020-12-19 22:31:15 +0200

    All states scraping added

--- myconstants.py
+++ myconstants.py
@@ -3,4 +3,9 @@ LAST_ELEMENT = -1
 FULL_EXTENSTION = 3
 HALF_URL = -15
 DB_FILENAME = 'usa_scraping_database'
-
+states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado,' 'Connecticut', 'Delaware', 'Florida',
+           'Georgia',  'Hawaii',  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
+           'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska',
+           'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico',  'New York', 'North Carolina', 'North Dakota', 'Ohio',
+           'Oklahoma','Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas',
+           'Utah', 'Vermont', 'Virginia','Washington', 'West Virginia', 'Wisconsin','Wyoming']
